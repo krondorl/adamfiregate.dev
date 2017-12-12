14583f091240ab1c6cf4ea6b80175d9897e49c55
Adam Burucs <adam.burucs@insdata.local>
2017-12-12 16:50:47 +0100

    Add styles and preconnect link

--- layout/header.php
+++ layout/header.php
@@ -2,6 +2,10 @@
 <!DOCTYPE html>
 <html lang="en" id="top">
 <head>
+  <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+  <link rel="preconnect" href="//i.ytimg.com" crossorigin>
+  <link rel="preconnect" href="//s.ytimg.com" crossorigin>
+  <link rel="preconnect" href="//www.youtube.com" crossorigin>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="<?php echo meta_desc(); ?>"/>
@@ -24,6 +28,11 @@
     p {
       margin: 0 0 1.5rem;
     }
+    hr {
+      background: none;
+      border: 2px solid purple;
+      height: 0;
+    }
     .group:after {
       content: "";
       display: table;
@@ -36,10 +45,27 @@
       margin: 0 1rem 1rem 0;
       max-width: 80%;
     }
+    .head-link-to-cover {
+      font-size: 1.5rem;
+      font-weight: bold;
+      line-height: 1.5;
+      text-decoration: none;
+    }
+    .head-link-to-cover * {
+      display: inline-block;
+      vertical-align: middle;
+    }
+    .head-link-to-cover span {
+      margin-left: 1rem;
+    }
     @media (min-width: 560px) {
       .profile-pic {
         float: left;
         max-width: 40%;
+      }
+      .head-link-to-cover {
+        font-size: 3rem;
+        line-height: 2;
       }
     }
     .center {
@@ -57,19 +83,6 @@
       max-width: 750px;
       padding: 0 .7rem;
     }
-    .head-link-to-cover {
-      font-size: 3rem;
-      font-weight: bold;
-      line-height: 2;
-      text-decoration: none;
-    }
-    .head-link-to-cover * {
-      display: inline-block;
-      vertical-align: middle;
-    }
-    .head-link-to-cover span {
-      margin-left: 1rem;
-    }
     a:link {
       color: purple;
     }
@@ -86,7 +99,7 @@
       font-size: 1rem;
     }
     [type=email] {
-      border: .2rem solid purple;
+      border: 2px solid purple;
       padding: 1rem;
     }
     .submit,
